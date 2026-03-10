9c75f806761d7778a0dfe7d3bcbab34da8ae75a5
Mirsaid Seyidli <seyidli.mirsaid@gmail.com>
2026-03-11 00:20:23 +0100

    Add files via upload

---
+++ obj/Exception/NegativeNumberException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Exceptions_Homework_06._12_25.Exception
+{
+    internal class NegativeNumberException : System.Exception
+    {
+        //public NegativeNumberException() { }
+        public NegativeNumberException() : base("Number is Negative (Menfi ededdir.)") {}
+        public NegativeNumberException(string message) : base("Aricmetic's operations was not chosen. (123)") {}
+    }
+}
