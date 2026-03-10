d4d6960ae7dcd0a2457bb5fcd895646a872ccc0a
Your Name <you@example.com>
2026-03-10 23:28:43 +0100

    initial commit

---
+++ DayOfWeek.cs
@@ -0,0 +1,13 @@
+﻿namespace Exceptions_06_12_25
+{
+    enum DayOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
